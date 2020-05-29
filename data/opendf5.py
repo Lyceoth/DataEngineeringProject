9adbe9da806b3ee7f38f7a24b987c01e3b3e1c85
topedi94 <60929262+topedi94@users.noreply.github.com>
2020-05-29 23:13:50 +0200

    Create Opendf5

---
+++ data/opendf5.py
@@ -0,0 +1,39 @@
+#!/bin/bash
+"true" '''\' #allow running from vs code, cf. http://rosettacode.org/wiki/Multiline_shebang#Python
+CUR_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CUR_FILE=`basename "$0"`
+CMD="docker run -it --rm -v \"$PWD/data:/data\" -v \"$CUR_DIR:/src\" --name=pyspark jupyter/pyspark-notebook spark-submit \"/src/$CUR_FILE\" | grep -v INFO"
+echo "Running: $CMD" | sed "s|$PWD|\$PWD|g"
+eval "$CMD"
+exit 0
+'''
+
+# BEGIN-SNIPPET
+from pyspark import SparkContext, SQLContext
+
+# Create local Spark Context
+spark = SparkContext("local[*]", "SQL_Soccer")
+sc = SQLContext(spark)
+
+# The following creates a DataFrame based on the content of a database table
+jdbcDF = spark.read
+  .format("jdbc")
+  .option("url", "jdbc:mysql:dbserver")
+  .option("dbtable", "schema.tablename")
+  .option("user", "username")
+  .option("password", "password")
+  .load()
+
+jdbcDF = spark.read.format("jdbc") \
+    .option("url", "jdbc:postgresql:dbserver") \
+    .option("dbtable", "schema.tablename") \
+    .option("user", "username").option("password", "password") \
+    .load()
+
+val df=spark.read.jdbc(url,"Player",prop) 
+
+# Displays the content of the DataFrame to stdout
+df.show()
+
+# Print the schema in a tree format
+df.printSchema()
