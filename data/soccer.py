657ab57ed97e74b2a22fb5661f52c90a79ce22d5
topedi94 <60929262+topedi94@users.noreply.github.com>
2020-05-31 20:05:03 +0200

    Delete not needed files

--- data/soccer.py
+++ data/soccer.py
@@ -12,21 +12,21 @@
 import re  # https://docs.python.org/3/library/re.html
 from pyspark.sql import SparkSession
 
-# Initialization
+# Initialization 
 
 sc = SparkSession.builder.appName('pyspark_soccer_app') \
-    .master("local[*]").getOrCreate().sparkContext
+    .master("local[*]").getOrCreate().sparkContext              # Create Spark Session Name: "pyspark_soccer_app"
 
 # Create an RDD from a text file
-sentences = sc.textFile("/data/wm.txt")
+sentences = sc.textFile("/data/wm.txt")                         # Creating Sentences from the text file "wm.txt" File contains comments of fifa world cup final 2014 (Germany vs. Argentina)
 
 # Run flat map, map, and reduceByKey
-words = sentences.flatMap(lambda sentence: sentence.split(" "))
-wordMap = words.map(lambda word: (word, 1))
+words = sentences.flatMap(lambda sentence: sentence.split(" ")) # Create single words out of the existing sentences
+wordMap = words.map(lambda word: (word, 1))                     
 reduceByKey = wordMap.reduceByKey(lambda a, b: a+b)
 
 # Print the individual results
 print(f"sentences: { sentences.collect() }")
 print(f"words: { words.collect() }")
 print(f"wordMap: { wordMap.collect() }")
-print(f"reduceByKey: { reduceByKey.collect() }")
+print(f"reduceByKey: { reduceByKey.collect() }")                # Show words within the docuent and the number they appear
