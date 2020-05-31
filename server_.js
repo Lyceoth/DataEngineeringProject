40007b5be717aef8c82152a72ba57ddd8e32f43a
A-Xander <58584903+A-Xander@users.noreply.github.com>
2020-05-31 03:09:02 +0200

    Update server_.js

--- server_.js
+++ server_.js
@@ -45,7 +45,7 @@ async function getFromCache(key) {
    return await memcached.get(key);
 }
 
-//Get data from database
+// Person DB Request
 async function getFromDatabase(userid) {
    let query = 'SELECT * from Player WHERE id = "' + userid + '" LIMIT 1';
    let session = await mysqlx.getSession(dbConfig);
@@ -62,8 +62,24 @@ async function getFromDatabase(userid) {
    }
 }
 
-// changed
-function send_response(response, data, cache_msg) {
+// Match DB Request
+async function getFromDatabase(matchid) {
+   let query = 'SELECT * from `Match` WHERE id = "' + matchid + '" LIMIT 1';
+   let session = await mysqlx.getSession(dbConfig);
+
+   console.log("Executing query " + query);
+   let res = await session.sql(query).execute();
+   let row = res.fetchAll();
+
+   if (row) {
+      console.log("Query result = ", row);
+      return row[0];
+   } else {
+      return null;
+   }
+}
+
+function send_match(response, data, cache_msg) {
    response.send(
       `<!DOCTYPE html>
    <head>
@@ -88,7 +104,7 @@ function send_response(response, data, cache_msg) {
             font-size: 1.2em;
             text-align:center;
         }
-        button {
+        .button {
         background-color: rgb(75, 75, 75);
         border-radius: 0.4em;
         border: none;
@@ -104,12 +120,99 @@ function send_response(response, data, cache_msg) {
         margin: 4% 2%;
         box-shadow: 0 0.1em #333;
         }
-        button:hover {
+        .button:hover {
             background-color: lightgray;
             color: rgb(41, 41, 41);
             text-shadow: 0.05em 0.05em rgb(204, 204, 204);
         }
-        button:active {
+        .button:active {
+            box-shadow: 0 0 #999;
+            transform: translateY(0.1em);
+        }
+        #data {
+            width: 20em;
+            margin:auto;
+            text-align:left;
+        }
+    </style>
+   </head>
+   <body>
+    <h1>Match: ${data[2]}</h1>
+    <ul id="data">
+        <li>Host: ${os.hostname()}</li>
+        <li>Date: ${new Date()}</li>
+        <li>Memcached Servers: ${memcachedServers}</li>
+        <li>Cache Status: ${cache_msg}</li>
+        <br>
+        <li>Match Stats</li>
+        <li>id: ${data[0]}</li>
+        <li>country_id: ${data[1]}</li>
+        <li>league_id: ${data[2]}</li>
+        <li>season: ${data[3]}</li>
+        <li>stage: ${data[4]}</li>
+        <li>${data[5]}</li>
+        <li>${data[6]}</li>
+        <li>${data[7]}</li>
+        <li>${data[8]}</li>
+        <li>${data[9]}</li>
+        <li>${data[10]}</li>
+        <br>
+        <li>Total Data: ${data}</li>
+    </ul>
+    <a class="button" href="https://10.0.2.15/person/${data[0]}" role="button">Show Player</a>
+    <a class="button" href="https://10.0.2.15/match/${data[0]}" role="button">Show Match</a>   
+   </body>
+   </html>`
+   );
+}
+
+function send_player(response, data, cache_msg) {
+   response.send(
+      `<!DOCTYPE html>
+   <head>
+    <meta charset="utf-8" />
+    <title>World Best Soccer App</title>
+    <link rel="shortcut icon"
+        href="https://raw.githubusercontent.com/Lyceoth/DataEngineeringProject/master/assets/football.png" />
+    <style>
+        h1 {
+            font: bold Verdana, Arial, sans-serif;
+            font-size: 2em;
+            margin: 4%;
+            color: white;
+            text-shadow: 0.05em 0.05em #333
+        }
+        body {
+            width: 100%;
+            height: 100%;
+            background-color: grey;
+            color: white;
+            text-shadow: 0.05em 0.05em #333;
+            font-size: 1.2em;
+            text-align:center;
+        }
+        .button {
+        background-color: rgb(75, 75, 75);
+        border-radius: 0.4em;
+        border: none;
+        color: white;
+        text-shadow: 0.05em 0.05em #333;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 1.2em;
+        width: 10em;
+        cursor: pointer;
+        margin: 4% 2%;
+        box-shadow: 0 0.1em #333;
+        }
+        .button:hover {
+            background-color: lightgray;
+            color: rgb(41, 41, 41);
+            text-shadow: 0.05em 0.05em rgb(204, 204, 204);
+        }
+        .button:active {
             box-shadow: 0 0 #999;
             transform: translateY(0.1em);
         }
@@ -139,12 +242,8 @@ function send_response(response, data, cache_msg) {
         <br>
         <li>Total Data: ${data}</li>
     </ul>
-    <button>
-        Previous
-    </button>
-    <button>
-        Next
-    </button>
+    <a class="button" href="https://10.0.2.15/person/${data[0]}" role="button">Show Player</a>
+    <a class="button" href="https://10.0.2.15/match/${data[0]}" role="button">Show Match</a>   
    </body>
    </html>`
    );
@@ -155,6 +254,29 @@ app.get("/", async function (request, response) {
    response.end();
 });
 
+// Match
+app.getAsync("/match/:id", async function (request, response) {
+   let matchid = request.params["id"];
+   let key = "match_" + matchid;
+   let cachedata = await getFromCache(key);
+
+   if (cachedata) {
+      console.log(`Cache hit for key=${key}, cachedata = ${cachedata}`);
+      send_match(response, cachedata, "Cache Hit");
+   } else {
+      console.log(`Cache miss for key=${key}, querying database`);
+      let data = await getFromDatabase(matchid);
+      if (data) {
+         console.log(`Got data=${data}, storing in cache`);
+         if (memcached) await memcached.set(key, data, 30 /* seconds */);
+         send_match(response, data, "Cache Miss");
+      } else {
+         send_match(response, "No data found", "No data found");
+      }
+   }
+});
+
+// Person
 app.getAsync("/person/:id", async function (request, response) {
    let userid = request.params["id"];
    let key = "user_" + userid;
@@ -162,16 +284,16 @@ app.getAsync("/person/:id", async function (request, response) {
 
    if (cachedata) {
       console.log(`Cache hit for key=${key}, cachedata = ${cachedata}`);
-      send_response(response, cachedata, "Cache Hit");
+      send_player(response, cachedata, "Cache Hit");
    } else {
       console.log(`Cache miss for key=${key}, querying database`);
       let data = await getFromDatabase(userid);
       if (data) {
          console.log(`Got data=${data}, storing in cache`);
          if (memcached) await memcached.set(key, data, 30 /* seconds */);
-         send_response(response, data, "Cache Miss");
+         send_player(response, data, "Cache Miss");
       } else {
-         send_response(response, "No data found", "No data found");
+         send_player(response, "No data found", "No data found");
       }
    }
 });
