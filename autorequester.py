3517d5db89447cd146bf09a4ebd794264b377f10
Lyceoth <lars@nebenfuehr.de>
2020-06-01 15:20:39 +0200

    Update autorequester.py

--- autorequester.py
+++ autorequester.py
@@ -7,10 +7,10 @@ import random
 import sys
 
 player_dist = {
-    7518: {"chance": 4, "count": 0}, 
-    1234: {"chance": 3, "count": 0},
-    2345: {"chance": 2, "count": 0},
-    3456: {"chance": 1, "count": 0} 
+    10243: {"chance": 4, "count": 0}, # Thomas Mueller
+    6556: {"chance": 3, "count": 0},  # Manuel Neuer
+    9039: {"chance": 2, "count": 0},  # Robert Lewandowski
+    5145: {"chance": 1, "count": 0}   #Jonas Hector
 }
 chances_list = []
 for p in player_dist.keys():
@@ -18,10 +18,14 @@ for p in player_dist.keys():
 
 
 def rand_playerid():
+    '''Get a random playerid according to the chances from player_dist'''
     return random.choice(chances_list)
     
 
 if __name__ == "__main__":
+    '''Sends multiple requests to the minikube public ip. If not specified as cmd-arg, the default amount is 40.
+       The requested playerid is randomized chosen based on chance for each player.'''
+
     parser = ArgumentParser()
     parser.add_argument("amount", nargs="?", default=40)
     n = int(parser.parse_args().amount)
